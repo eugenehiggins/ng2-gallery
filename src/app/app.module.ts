93b083d8a85c56d96d921a6cb15cf36810d6dcd2
Gene Higgins <ehigginsiii@cnm.edu>
2017-02-28 15:24:36 -0700

    added angularfire config code block

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,23 +9,34 @@ import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
 import { AlbumComponent } from './components/album/album.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ContentService } from "./services/content.service";
+import { AngularFireModule } from 'angularfire2';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyAx0Wo5qiYzps_kReaCLr4AzlhYLFvK-MU",
+    authDomain: "gene-higgins.firebaseapp.com",
+    databaseURL: "https://gene-higgins.firebaseio.com",
+    storageBucket: "gene-higgins.appspot.com",
+    messagingSenderId: "557188003562"
+};
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    JumbotronComponent,
-    AlbumComponent,
-    FooterComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [
-    ContentService
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavbarComponent,
+        JumbotronComponent,
+        AlbumComponent,
+        FooterComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        AngularFireModule.initializeApp(firebaseConfig)
+    ],
+    providers: [
+        ContentService
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
