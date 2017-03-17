5fbaa12cfec1cc360b85f862057c10fde8fd7dad
Gene Higgins <genotype@gknow.com>
2017-03-17 16:51:55 -0600

    can upload files and retrieve download url

--- src/app/dashboard/components/file-upload/file-upload.component.ts
+++ src/app/dashboard/components/file-upload/file-upload.component.ts
@@ -1,34 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-import { Headers } from "@angular/http";
+import { FirebaseApp } from 'angularfire2';
 
 @Component({
-  selector: 'file-upload',
-  templateUrl: './file-upload.component.html',
-  styleUrls: ['./file-upload.component.scss']
+    selector: 'file-upload',
+    templateUrl: './file-upload.component.html',
+    styleUrls: ['./file-upload.component.scss']
 })
 export class FileUploadComponent implements OnInit {
 
-  constructor(public fb: FormBuilder) { }
+    _firebase: any;
+
+    constructor(public fb: FormBuilder, @Inject(FirebaseApp) firebaseApp: any) {
+        this._firebase = firebaseApp;
+    }
 
     public imageUploadForm = this.fb.group({
-        name: [ "", Validators.required ],
-        description: [ "", Validators.required ],
-        imagePath: [ "", Validators.required ]
+        name: ["", Validators.required],
+        description: ["", Validators.required],
+        imagePath: ["", Validators.required]
     });
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  onSave(event) {
-      // TODO: only assuming single files right now
-      let file: File = event.target.imagePath.files;
-      let formData:FormData = new FormData();
-      formData.append('loadFile', file, file.name);
-      let headers = new Headers();
-      headers.append('Content-Type', 'multipart/form-data');
-      headers.append('Accept', 'application')
-     console.log( );
-  }
+    onSave(event) {
+        // TODO: only assuming single files right now
+        // this might help http://stackoverflow.com/questions/40214772/file-upload-in-angular-2
+        //http://www.muhammetbakan.com/angular2-ionic2-how-to-upload-and-get-image-from-firebase-storage/
+
+        // get file
+        let file: File = event.target.imagePath.files[0];
+        let storage = this._firebase.storage();
+        let storageRef = storage.ref();
+
+        // create metadata
+        let metadata = {
+            contentType: "image/jpeg"
+        }
+
+        let promise = new Promise((res, rej) => {
+            let uploadTask = this._firebase.storage().ref('images/' + file.name)
+                .put(file, metadata);
+            uploadTask.on('state_changed', function (snapshot) {
+                console.log('file uploaded')
+            }, function (error) {
+                rej(error);
+            }, function () {
+                let downloadURL = uploadTask.snapshot.downloadURL;
+                console.log(downloadURL);
+                res(downloadURL);
+            });
+
+        });
+        return promise;
+    }
 
 }
